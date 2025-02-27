1ec60c1190c470dc262a9c9b1963ab592daa9f92
KelseyF624 <kelseyf.624@gmail.com>
2025-02-27 17:21:26 -0500

    Partial Commit

--- src/CrapsGame.java
+++ src/CrapsGame.java
@@ -2,29 +2,28 @@ import java.util.Random;
 import java.util.Scanner;
 public class CrapsGame {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Random random = new Random();
-        int roll = random.nextInt(12) + 2;
-        int rollAgain = random.nextInt(12) + 2;
-        int crapsRoll;
+        int roll = (int) (Math.random()* 12 + 2);
+        int rollAgain = (int) (Math.random()* 12 + 2);
         String playerRoll= "R" ;
         boolean playAgain = true;
-        Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the Craps Game!");
         System.out.println("To win roll a 7 or 11.");
         do {
             do {
                 System.out.println("Press R to roll.");
                 playerRoll = sc.nextLine();
-                crapsRoll = roll;
-                System.out.println("Roll is: " + crapsRoll);
-                if (crapsRoll == 2 || crapsRoll == 3 || crapsRoll == 12) {
+                roll = roll;
+                System.out.println("Roll is: " + roll);
+                if (roll == 2 || roll == 3 || roll == 12) {
                     System.out.println("You crapped out! You LOST!");
                 }
-                if (crapsRoll == 7 || crapsRoll == 11) {
+                if (roll == 7 || roll == 11) {
                     System.out.println("You rolled a natural! You WIN!");}
-                if (crapsRoll == 4 || crapsRoll == 5 || crapsRoll == 6 || crapsRoll == 8 || crapsRoll == 9 || crapsRoll == 10); {
-                rollAgain = roll;}
-            } while (crapsRoll == 4 || crapsRoll == 5 || crapsRoll == 6 || crapsRoll == 8 || crapsRoll == 9 || crapsRoll == 10);
+                if (roll == 4 || roll == 5 || roll == 6 || roll == 8 || roll == 9 || roll == 10); {
+                rollAgain = roll; System.out.println("You're trying for a point. Roll Again!");}
+            } while (roll == 4 || roll == 5 || roll == 6 || roll == 8 || roll == 9 || roll == 10);
             System.out.println ("Play again? (Y/N)");
             playAgain = sc.nextLine().equalsIgnoreCase("Y");
         } while (playAgain);
