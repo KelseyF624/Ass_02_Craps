3647b38df8d660611d2b4aea9aaa43d85f79ecc9
KelseyF624 <kelseyf.624@gmail.com>
2025-02-27 17:47:49 -0500

    Partial Commit

--- src/CrapsGame.java
+++ src/CrapsGame.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class CrapsGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Random random = new Random();
-        int roll = (int) (Math.random()* 12 + 2);
-        int rollAgain = (int) (Math.random()* 12 + 2);
-        String playerRoll= "R" ;
+        int roll, rollAgain;
+        String playerRoll;
         boolean playAgain = true;
         System.out.println("Welcome to the Craps Game!");
         System.out.println("To win roll a 7 or 11.");
         do {
+            int die1 = (int) (Math.random()* 6 + 1);
+            int die2 = (int) (Math.random()* 6 + 1);
             do {
                 System.out.println("Press R to roll.");
                 playerRoll = sc.nextLine();
-                roll = roll;
+                roll = die1 + die2;
                 System.out.println("Roll is: " + roll);
                 if (roll == 2 || roll == 3 || roll == 12) {
                     System.out.println("You crapped out! You LOST!");
@@ -22,7 +22,7 @@ public class CrapsGame {
                 if (roll == 7 || roll == 11) {
                     System.out.println("You rolled a natural! You WIN!");}
                 if (roll == 4 || roll == 5 || roll == 6 || roll == 8 || roll == 9 || roll == 10); {
-                rollAgain = roll; System.out.println("You're trying for a point. Roll Again!");}
+                System.out.println("You're trying for a point. Roll Again!");}
             } while (roll == 4 || roll == 5 || roll == 6 || roll == 8 || roll == 9 || roll == 10);
             System.out.println ("Play again? (Y/N)");
             playAgain = sc.nextLine().equalsIgnoreCase("Y");
